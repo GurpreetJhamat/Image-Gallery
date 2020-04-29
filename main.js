04e65f22f52a42aaee12f93cc731dfb94b463f0d
GurpreetJhamat <gjhammt@gmail.com>
2020-04-28 20:36:17 -0400

    images fixed

--- main.js
+++ main.js
@@ -2,14 +2,10 @@ const current = document.querySelector('#img01');
 const imgs = document.querySelectorAll('.image img');
 const image = document.querySelector('.img_container');
 const addClass = document.querySelectorAll('.over');
-// const opacity = 0.5;
-// console.log(current)
-console.log(image.className)
-console.log(addClass)
-console.log(addClass[0].className)
-function on() {
+
+function on(e) {
     document.getElementById("image_viewer").style.display = "block"; 
-    console.log('working on function');  
+    console.log('working on function'); 
 }
 
 function off() {
@@ -17,16 +13,21 @@ function off() {
 }
 
 const imgClick = (e) => {
-
     current.src = e.target.src;
     image.classList.add(e.srcElement.classList.item(0));
-    console.log('working');
-    console.log(e);
-    console.log(image.className);
-    console.log(e.src.className);
+    // console.log(e);
+    const clsFound = e.path[2].classList.item(0);
+        if (clsFound == 'big' || clsFound == 'big-b'){
+            image.style.width = "50%";
+            // console.log('BIG')
+        } else if(clsFound == 'vertical') {
+            image.style.width = "25%";
+        } else if (clsFound == 'horizontal' || clsFound == 'horizontal-h'){
+            image.style.width = "90%";
+        } else{
+            image.style.width = "50%";
+        }
+      }
     
-    // imgs.style.opacity = 1;
-}
-
 imgs.forEach(img => img.addEventListener('click', imgClick));
 imgs.forEach(img => img.addEventListener('click', on));
